add625515da96d93ede44a3c3589596937945d56
viettt0119 <viettt0119@gmail.com>
2024-01-17 09:46:08 +0700

    commit note

---
+++ ptbac1/src/giaiptbac1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class giaiptbac1 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args)  {
+        int a , b;
+        double nghiempt;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Nhap a: ");
+        a = sc.nextInt();
+        System.out.println("Nhap b:");
+        b=sc.nextInt();
+        if(a==0){
+            if(b==0){
+                System.out.println("Phuong trinh co vo so nghiem ");
+            }
+            else {
+            System.out.println("Phuong trinh vo nghiem ");
+            }
+        }else {
+            nghiempt= (double) -b/a; 
+        System.out.println("Phuong trinh co mot nghiem duy nhat :" +(nghiempt) ++ );
+        }
+    }
+}
+
